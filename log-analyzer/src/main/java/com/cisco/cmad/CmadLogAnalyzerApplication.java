4b8e7d25e67b315d7704b845711361d614a9dc5e
fkhader <fkhader@cisco.com>
2020-01-28 12:21:23 +0530

    Initial commit: CMAD log analyzer spring boot project.

---
+++ log-analyzer/src/main/java/com/cisco/cmad/CmadLogAnalyzerApplication.java
@@ -0,0 +1,15 @@
+package com.cisco.cmad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmadLogAnalyzerApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(CmadLogAnalyzerApplication.class, args);
+		
+	}
+
+}
