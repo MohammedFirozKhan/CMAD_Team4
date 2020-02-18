480313a3806726658d516646d426836b35049479
fkhader <fkhader@cisco.com>
2020-02-18 20:20:44 +0530

    Added date format to @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")

--- log-analyzer/src/main/java/com/cisco/cmad/jpa/entity/Log.java
+++ log-analyzer/src/main/java/com/cisco/cmad/jpa/entity/Log.java
@@ -7,31 +7,22 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
 @Entity
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "logId","logLevel", "logMsg", "processName", "hostName", "date" })
 public class Log {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@JsonProperty("logId")
 	private Long logId;
-	@JsonProperty("logLevel")
 	private String logLevel;
-	@JsonProperty("logMsg")
 	private String logMsg;
-	@JsonProperty("processName")
 	private String processName;
-	@JsonProperty("hostName")
 	private String hostName;
-	@JsonProperty("date")
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
 	private Date date;
 	
 
